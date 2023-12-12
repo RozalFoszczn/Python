695cd5f020a600f1f08c14a2e2cc0eb88d30cc96
RozalFoszczn <r.foszczn@gmail.com>
2023-12-12 10:24:16 +0100

    u

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+
+
 import time
 print("witaj.Za chwile rozpocznie sie twoj test szybkiego pisania.")
 st=int(input("wpisz 1, kiedy bedziesz gotow: "))
@@ -7,6 +9,36 @@ if st == 1:
     if i=="Wszystkie komputery powinny miec na tapetach ryby":
         end=time.time()
         print(end-start)
+        a=(end-start)
+        if a<=6:
+            print("Piszesz niczym profesjonalista!")
+        elif a<=7:
+            print("Piszesz bardzo szybko")
+        elif a<=8:
+            print("Nie jest zle")
+        elif a<=9:
+            print("moze sproboj jeszcze raz")
+        else:
+            print("potrzebujesz jeszcze pocwiczyc")
     else:
         print("zle przepisales tekst. Sproboj jeszcze raz")
-    if end-start==
+    str=int(input("wpisz 1, kiedy bedziesz gotow: "))
+    if str == 1:
+        start=time.time()
+        i=input("przepisz tekst - Male ryby nurkuja w glebokiej i slodkiej wodzie: ")
+        if i=="Male ryby nurkuja w glebokiej i slodkiej wodzie":
+            end=time.time()
+            print(end-start)
+            a=(end-start)
+            if a<=6:
+                print("Piszesz niczym profesjonalista!")
+            elif a<=7:
+                print("Piszesz bardzo szybko")
+            elif a<=8:
+                print("Nie jest zle")
+            elif a<=9:
+                print("moze sproboj jeszcze raz")
+            else:
+                print("potrzebujesz jeszcze pocwiczyc")
+        else:
+            print("Znowu zle przepisales tekst. sproboj zwrocic na to wieksza uwage")
