936fb87a00798c13a85c1f081dc3a71546af3613
RozalFoszczn <r.foszczn@gmail.com>
2024-01-20 18:24:36 +0100

    n

--- wisielec.py
+++ wisielec.py
@@ -6,10 +6,22 @@ while play:
     if wyb=="jedno":
 
         import random
+        
 
         print("Witaj graczu. Twoim zadaniem bedzie odgadnac losowe haslo. W haslach nie ma duzych liter i polskich znakow.")
-
-        lista = ["romus", "luminescencja", "magnetoelektryczny", "malkontenctwo", "primaaprilisowy", "kociak", "zwierzchnictwo", "lumpenproletariat", "deoksyrybonukleinowy", "krztusiec", "prokrastynacja", "ekwilibrystyka", "transcendencja", "somnambulizm", "tromtadracja", "plenipotencja", "dezynwoltura", "poliandria", "kuriozum", "luminarz", "mizogin", "egzegeza", "arywizm", "ryba", "wyimaginowany", "imponderabilia", "metamorfozowanie", "malkontent", "emulgacja", "oksymoron", "ekstrapolacja", "europarlamentarzysta", "ortodoksyjny", "apartamentowiec", "ekwilibrystyka", "franczyzobiorca", "ambiwalentny", "konfabulacja", "wyselekcjonowany", "niezdyscyplinowanie", "dezaprobatywnie", "ekstraterestrialny", "kontrrewolucjonista", "parafrazowanie", "ksylografowanie", "konserwatywnie", "superpozycjonowanie", "zindywidualizowany", "dekoncentracja", "eksterminacyjny", "monopolizacyjny", "wzajemnopomocowy", "dekonstrukcjonizm", "egzemplifikacyjny", "znormalizowany", "egzekucjonowanie", "bibliomaniak", "kameleoniczny", "oksfordzki"]
+        wybpz=int(input("jaki poziom trudnosci wybierasz(1 -15 zyc, 2 -10 zyc, 3 -5 zyc, 4 -(poziom ryba) 1 zycie)"))
+        if wybpz==1:
+            zycia=15
+        elif wybpz==2:
+            zycia=10
+        elif wybpz==3:
+            zycia=5
+        elif wybpz==4:
+            zycia=1
+        else:
+            print("wpisales zla liczbe")
+            break
+        lista = ["makrela", "szprot", "dorsz", "halibut", "karp", "lin", "sandacz", "jesiotr", "brzana", "sardynka", "karmazyn", "turbot", "sola", "morszczuk", "tilapia", "mintaj", "kergulena", "panga", "dorada", "romus", "luminescencja", "magnetoelektryczny", "malkontenctwo", "primaaprilisowy", "kociak", "zwierzchnictwo", "lumpenproletariat", "deoksyrybonukleinowy", "krztusiec", "prokrastynacja", "ekwilibrystyka", "transcendencja", "somnambulizm", "tromtadracja", "plenipotencja", "dezynwoltura", "poliandria", "kuriozum", "luminarz", "mizogin", "egzegeza", "arywizm", "ryba", "wyimaginowany", "imponderabilia", "metamorfozowanie", "malkontent", "emulgacja", "oksymoron", "ekstrapolacja", "europarlamentarzysta", "ortodoksyjny", "apartamentowiec", "ekwilibrystyka", "franczyzobiorca", "ambiwalentny", "konfabulacja", "wyselekcjonowany", "niezdyscyplinowanie", "dezaprobatywnie", "ekstraterestrialny", "kontrrewolucjonista", "parafrazowanie", "ksylografowanie", "konserwatywnie", "superpozycjonowanie", "zindywidualizowany", "dekoncentracja", "eksterminacyjny", "monopolizacyjny", "wzajemnopomocowy", "dekonstrukcjonizm", "egzemplifikacyjny", "znormalizowany", "egzekucjonowanie", "bibliomaniak", "kameleoniczny", "oksfordzki"]
 
         haslo = str(lista[random.randint(0, len(lista) - 1)])
         tablica = list(haslo)
@@ -18,7 +30,6 @@ while play:
         for i in range(len(haslo)):
             tablica[i] = "_"
 
-        zycia = 10
 
         while zycia > 0:
             print("")
